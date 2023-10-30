0d07f8857cc02ec57017f89ebb58b23821954b96
Ethan Uppal <113849268+ethanuppal@users.noreply.github.com>
2023-10-30 10:54:16 -0400

    initial step for window

---
+++ src/main.c
@@ -0,0 +1,68 @@
+// Copyright (C) 2023 Ethan Uppal. All rights reserved.
+
+#include <stdio.h> // fprintf, stderr
+#include <stdlib.h> // malloc, realloc
+#include <stddef.h> // NULL
+#include <stdint.h> // uint32_t
+#include "MiniFB.h"
+
+#define BYTES_PER_PIXEL 4
+#define WINDOW_TITLE "Robot Simulator"
+
+static struct {
+    uint32_t width;
+    uint32_t height;
+    uint32_t *buffer;
+} ctx = {
+    .width  = 800,
+    .height = 600,
+    .buffer = NULL
+};
+
+// resize callback
+void resize(struct mfb_window* window, int width, int height) {
+    ctx.width = width;
+    ctx.height = height;
+    ctx.buffer = realloc(ctx.buffer, ctx.width * ctx.height * BYTES_PER_PIXEL);
+}
+
+int main() {
+    // create window
+    struct mfb_window* window = mfb_open_ex(WINDOW_TITLE, ctx.width, ctx.height, 
+        WF_RESIZABLE);
+    if (!window) {
+        fprintf(stderr, "Failed to open window\n");
+        return 1;
+    }
+    ctx.buffer = malloc(ctx.width * ctx.height * BYTES_PER_PIXEL);
+
+    // configure window
+    mfb_set_resize_callback(window, resize);
+
+    // main loop
+    mfb_update_state state;
+    do {
+        state = mfb_update_ex(window, ctx.buffer, ctx.width, ctx.height);
+        if (state != STATE_OK) {
+            break;
+        }
+    } while (mfb_wait_sync(window));
+
+    // handle exit state
+    switch (state) {
+        case STATE_EXIT:
+            return 0; // exit early on success
+        case STATE_INVALID_WINDOW:
+            fprintf(stderr, "Invalid window\n");
+            break;
+        case STATE_INVALID_BUFFER:
+            fprintf(stderr, "Invalid buffer\n");
+            break;
+        case STATE_INTERNAL_ERROR:
+            fprintf(stderr, "Internal error\n");
+        default:
+            fprintf(stderr, "Unknown error\n");
+            break;
+    }
+    return 1;
+}
